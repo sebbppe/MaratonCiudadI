2df4d2b856da6eebbefc93b419c9ec426be3d9f4
sebbppe <sebbppe@gmail.com>
2022-10-14 16:04:01 -0500

    ya casi

--- Maraton/gestionDatos/models.py
+++ Maraton/gestionDatos/models.py
@@ -1,3 +1,32 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Calle(models.Model):
+    tipo=models.CharField(max_length=50)
+    numero=models.CharField(max_length=50)
+    orientacion=models.CharField(max_length=50)
+class Carrera(models.Model):
+    tipo=models.CharField(max_length=50)
+    numero=models.CharField(max_length=50)
+    orientacion=models.CharField(max_length=50)
+class Via(models.Model):
+    calle=models.ForeignKey(Calle,on_delete=models.CASCADE)
+    carrera=models.ForeignKey(Carrera,on_delete=models.CASCADE)
+    
+class Propietario(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+class Vehiculo(models.Model):
+    tipo=models.CharField(max_length=50)
+    capacidad=models.IntegerField(max_length=5)
+    placa=models.CharField(max_length=50)
+    propietatio=models.ForeignKey(Propietario,on_delete=models.CASCADE)
+class semaforo(models.Model):
+    tipoA=models.CharField(max_length=50)
+    numA=models.IntegerField(max_length=10)
+    tipoB=models.CharField(max_length=50)
+    numB=models.IntegerField(max_length=10)
+    estado=models.CharField(max_length=10)
+    
--- Maraton/gestionDatos/views.py
+++ Maraton/gestionDatos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import Template,Context
+from django.template import loader
+from django.shortcuts import render
+
+def malladoVial(request):
+    lista=["Sebastián","Fercho","Lisseth"]
+    variable=98
+    return render(request,"index.html",{"Variable":variable,"Nombres":lista})
